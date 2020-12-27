fec69959ba3398df34946e751a82df9e650c62d9
toumasaya <luffy.w.pirate@gmail.com>
2020-12-27 17:54:03 +0800

    feat: Refactor MemoriesPage

---
+++ src/components/memories/MemoriesList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ion-list>
+    <memories-item
+      v-for="memory in memories"
+      :key="memory.id"
+      :memory="memory"
+    ></memories-item>
+  </ion-list>
+</template>
+
+<script>
+import { IonList } from "@ionic/vue";
+import MemoriesItem from "./MemoriesItem";
+
+export default {
+  props: ["memories"],
+  components: {
+    IonList,
+    MemoriesItem,
+  },
+};
+</script>
--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,30 +1,15 @@
 <template>
   <base-layout pageTitle="All Memories">
-    <ion-list>
-      <ion-item
-        v-for="memory in memories"
-        :key="memory.id"
-        :router-link="`/memories/${memory.id}`"
-      >
-        <ion-thumbnail slot="start">
-          <ion-img :src="memory.image" :alt="memory.title" />
-        </ion-thumbnail>
-        <ion-label>{{ memory.title }}</ion-label>
-      </ion-item>
-    </ion-list>
+    <memories-list :memories="memories"></memories-list>
   </base-layout>
 </template>
 
 <script>
-import { IonList, IonItem, IonImg, IonThumbnail, IonLabel } from "@ionic/vue";
+import MemoriesList from "@/components/memories/MemoriesList.vue";
 
 export default {
   components: {
-    IonList,
-    IonItem,
-    IonImg,
-    IonThumbnail,
-    IonLabel,
+    MemoriesList,
   },
   computed: {
     memories() {
